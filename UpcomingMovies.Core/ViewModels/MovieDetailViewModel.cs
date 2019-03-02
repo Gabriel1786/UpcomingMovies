1ab041d12323469abe02fb12830a301c17ad39f6
Gabriel den Hartog <gabriel1786@gmail.com>
2019-03-02 12:22:41 -0300

    feat: loading movies on initialize, some control on currentPage added

--- UpcomingMovies.Core/ViewModels/MovieDetailViewModel.cs
+++ UpcomingMovies.Core/ViewModels/MovieDetailViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+using UpcomingMovies.Core.Models;
 
 namespace UpcomingMovies.Core.ViewModels
 {
-    public class MovieDetailViewModel : BaseViewModel
+    public class MovieDetailViewModel : BaseViewModel, IMvxViewModel<Movie>
     {
         readonly IMvxNavigationService _navigationService;
+        Movie _movie;
 
         public MovieDetailViewModel(IMvxNavigationService navigationService)
         {
@@ -13,6 +16,10 @@ namespace UpcomingMovies.Core.ViewModels
         }
 
         // MvvmCross Lifecycle
+        void IMvxViewModel<Movie>.Prepare(Movie parameter)
+        {
+            _movie = parameter;
+        }
 
         // MVVM Properties
 
--- UpcomingMovies.Core/ViewModels/MoviesViewModel.cs
+++ UpcomingMovies.Core/ViewModels/MoviesViewModel.cs
@@ -1,23 +1,84 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MvvmCross.Commands;
 using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+using UpcomingMovies.Core.Models;
+using UpcomingMovies.Core.Services;
 
 namespace UpcomingMovies.Core.ViewModels
 {
     public class MoviesViewModel : BaseViewModel
     {
         readonly IMvxNavigationService _navigationService;
+        readonly IMovieService _movieService;
 
-        public MoviesViewModel(IMvxNavigationService navigationService)
+        int _currentPage = 1;
+        bool _canLoadMore = true;
+
+        public MoviesViewModel(IMvxNavigationService navigationService, IMovieService movieService)
         {
             _navigationService = navigationService;
+            _movieService = movieService;
+
+            Movies = new MvxObservableCollection<Movie>();
+
+            ShowMovieDetailViewModelCommand = new MvxAsyncCommand<Movie>(ShowMovieDetailView);
+            LoadMoreMoviesCommand = new MvxCommand(() =>
+            {
+                LoadMoreMoviesTask = MvxNotifyTask.Create(LoadMovies);
+                RaisePropertyChanged(() => LoadMoreMoviesTask);
+            });
         }
 
         // MvvmCross Lifecycle
+        public override Task Initialize()
+        {
+            var task = base.Initialize();
+            LoadInitialMoviesTask = MvxNotifyTask.Create(LoadMovies);
+            return task;
+        }
 
         // MVVM Properties
+        MvxObservableCollection<Movie> _movies;
+        public MvxObservableCollection<Movie> Movies
+        {
+            get => _movies;
+            set => SetProperty(ref _movies, value);
+        }
+
+        public MvxNotifyTask LoadInitialMoviesTask { get; private set; }
+        public MvxNotifyTask LoadMoreMoviesTask { get; private set; }
 
         // MVVM Commands
+        public IMvxAsyncCommand<Movie> ShowMovieDetailViewModelCommand { get; }
+        public IMvxCommand LoadMoreMoviesCommand { get; }
 
         // Private Methods
+        async Task LoadMovies()
+        {
+            if (!_canLoadMore)
+                return;
+
+            var responseInfo = await _movieService.GetMoviesAsync(MovieListType.Upcoming, new Dictionary<string, object>
+            {
+                { "page", _currentPage }
+            });
+
+            if (responseInfo.IsSuccess)
+            {
+                Movies.AddRange(responseInfo.Result.Movies);
+
+                _currentPage++;
+                if (_currentPage > responseInfo.Result.TotalPages)
+                    _canLoadMore = false;
+            }
+        }
+
+        async Task ShowMovieDetailView(Movie movie)
+        {
+            await _navigationService.Navigate<MovieDetailViewModel, Movie>(movie);
+        }
     }
 }
