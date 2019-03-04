fe128841a44b8167cfbb3e7398d6f3402152a4d2
Gabriel den Hartog <gabriel1786@gmail.com>
2019-03-03 22:57:17 -0300

    fix: calculating # of columns based on device screen size

--- UpcomingMovies.Forms.UI/Views/MoviesPage.xaml.cs
+++ UpcomingMovies.Forms.UI/Views/MoviesPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using MvvmCross.Forms.Presenters.Attributes;
+﻿using System;
+using MvvmCross.Forms.Presenters.Attributes;
 using MvvmCross.Forms.Views;
 using UpcomingMovies.Core.ViewModels;
+using Xamarin.Essentials;
 
 namespace UpcomingMovies.Forms.UI.Views
 {
@@ -16,7 +18,21 @@ namespace UpcomingMovies.Forms.UI.Views
 
         void SetupCollectionView()
         {
-            //TODO: Determine right amount of columns depending on device screen
+            var desiredCellWidth = 160;
+
+            DisplayInfo info = DeviceDisplay.MainDisplayInfo;
+            if (info.Orientation == DisplayOrientation.Portrait)
+            {
+                var dipWidth = info.Width / info.Density;
+                var numberOfCellsP = dipWidth / desiredCellWidth;
+
+                collectionView.PortraitColumns = (int)Math.Floor(numberOfCellsP);
+
+                var dipHeight = info.Height / info.Density;
+                var numberOfCellsL = dipHeight / desiredCellWidth;
+
+                collectionView.LandscapeColumns = (int)Math.Floor(numberOfCellsL);
+            }
         }
     }
 }
